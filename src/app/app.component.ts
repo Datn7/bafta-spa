11ec167600e61d4f2f66691b404b4d412636205b
Datn7 <datunashvili72@gmail.com>
2021-09-13 15:21:55 +0400

    initCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination.model';
+import { IProduct } from './models/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bafta-spa';
+
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit(): void {}
 }
